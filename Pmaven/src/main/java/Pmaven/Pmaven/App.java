6f5b1626d6e4a6629e980955366abd1e42d88814
ajc <ajc@admin-PC>
2018-02-13 10:10:32 +0100

    message essai pmaven

---
+++ Pmaven/src/main/java/Pmaven/Pmaven/App.java
@@ -0,0 +1,13 @@
+package Pmaven.Pmaven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
